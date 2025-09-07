13ed59f0157ba24a9ade3b7ab59982ab3709ca61
LucasKonrath <lucaskdamaceno@gmail.com>
2025-09-07 15:29:32 -0300

    add contract verifier poc

---
+++ contract-verifier-poc/src/main/java/com/example/contract_verifier_poc/ContractVerifierPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.contract_verifier_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractVerifierPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractVerifierPocApplication.class, args);
+	}
+
+}
---
+++ contract-verifier-poc/src/test/java/com/example/contract_verifier_poc/ContractVerifierPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contract_verifier_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractVerifierPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
