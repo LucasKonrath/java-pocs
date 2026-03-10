b17003f1ca969a248cb890a715dd95e5a8d174fb
LucasKonrath <lucaskdamaceno@gmail.com>
2026-03-10 00:15:02 -0700

    add initial project setup with Spring Boot and Maven wrapper

---
+++ bean-scopes/src/main/java/org/example/beanscopes/BeanScopesApplication.java
@@ -0,0 +1,14 @@
+package org.example.beanscopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanScopesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanScopesApplication.class, args);
+        int i = 0;
+    }
+
+}
---
+++ spring-setter-constructor-injection/src/main/java/org/example/springsetterconstructorinjection/SpringSetterConstructorInjectionApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsetterconstructorinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSetterConstructorInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSetterConstructorInjectionApplication.class, args);
+	}
+
+}
